a2d9b2ef02f3878bd9c797292de87a1f03f67578
lbq <lbq@qq.com>
2016-06-22 19:15:47 +0800

    导入

---
+++ 01彩票项目(基本搭建)/彩票项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  彩票项目
+//
+//  Created by 卢卢柏全 on 16/5/1.
+//  Copyright © 2016年 LuBaiQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
